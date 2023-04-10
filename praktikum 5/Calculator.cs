6f0e8d7c604a945b629df814fef94d9881f1476a
kukuhsetiadi221 <kukuhsetiadi@students.amikom.ac.id>
2023-04-10 22:02:51 +0700

    Add project files.

---
+++ praktikum 5/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktikum_5
+{
+    public class Calculator
+    {
+
+        public int CariNilai(int a, int b, int c)
+        {
+            int min;
+            if (a < b)
+            {
+                Console.WriteLine("Nilai min dari ketiga nilai adalah : {0}", min = a);
+                Console.ReadKey();
+            }
+            else if (b < c)
+            {
+                Console.WriteLine("Nilai min dari ketiga nilai adalah : {0}", min = b);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Nilai min dari ketiga nilai adalah : {0}", min = c);
+                Console.ReadKey();
+            }
+            return min;
+        }
+    }
+    public class Calkulator
+    {
+        public int CariNilai(int a, int b, int c)
+        {
+            int max;
+            if (a > b)
+            {
+                Console.WriteLine("Nilai max dari ketiga nilai adalah : {0}", max = a);
+                Console.ReadKey();
+            }
+            else if (b > c)
+            {
+                Console.WriteLine("Nilai max dari ketiga nilai adalah : {0}", max = b);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Nilai max dari ketiga nilai adalah : {0}", max = c);
+                Console.ReadKey();
+            }
+            return max;
+        }
+    }
+}
---
+++ praktikum 5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktikum_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator1 = new Calculator();
+            Calkulator calculator2 = new Calkulator();
+            Console.Write(calculator1.CariNilai(6, 4, 5));
+            Console.Write(calculator2.CariNilai(7, 5, 3));  
+            Console.ReadKey();
+        }
+    }
+}
